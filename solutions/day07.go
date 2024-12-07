613b89bc6798c83848d8b91d2589a369fbaf75fc
Simone Vortice <simone.vortice@proton.me>
2024-12-07 13:46:03 +0100

    day 7

--- solutions/day07.go
+++ solutions/day07.go
@@ -3,9 +3,20 @@ package solutions
 import (
 	"fmt"
 	"os"
-	"sort"
+	"strconv"
 	"strings"
 )
+
+type Operator int
+
+const (
+	PLUS Operator = iota
+	MULT
+	CONCAT
+)
+
+var operators_p1 []Operator = []Operator{PLUS, MULT}
+var operators_p2 []Operator = []Operator{PLUS, MULT, CONCAT}
 
 func Day07() {
 	const input_file = "inputs/day07.txt"
@@ -30,18 +41,133 @@ func Day07() {
 	fmt.Printf("\tPart 2: %d\n", sol_2)
 }
 
+func parse_result_values(l string) (int, []int) {
+	split := strings.SplitN(l, ": ", 2)
+	result, err := strconv.Atoi(split[0])
+	if err != nil {
+		fmt.Println("Error converting result")
+		os.Exit(1)
+	}
+	values := mapfunc(strings.Split(split[1], " "), func(s string) int {
+		v, err := strconv.Atoi(s)
+		if err != nil {
+			fmt.Println("Error converting values")
+			return 0
+		}
+		return v
+	})
+	return result, values
+}
+
+func apply_operator(a int, b int, op Operator) int {
+	var result int
+	switch op {
+	case PLUS:
+		{
+			result = a + b
+		}
+	case MULT:
+		{
+			result = a * b
+		}
+	case CONCAT:
+		{
+			a_str := strconv.Itoa(a)
+			b_str := strconv.Itoa(b)
+			c, err := strconv.Atoi(a_str + b_str)
+			if err != nil {
+				return 0
+			}
+			result = c
+		}
+	default:
+		panic("WTF")
+	}
+	return result
+}
+
+func eval_expression(values []int, operators []Operator) (int, error) {
+	if len(values) != len(operators)+1 {
+		return 0, fmt.Errorf("Numbers of values and operators are not compatible")
+	}
+	if len(values) == 2 {
+		return apply_operator(values[0], values[1], operators[0]), nil
+	}
+	result, err := eval_expression(values[:len(values)-1], operators[:len(operators)-1])
+	return apply_operator(result, values[len(values)-1], operators[len(operators)-1]), err
+}
+
+func generate_permutations[T any](elements []T, length int) [][]T {
+	var result [][]T
+	var permutation []T
+
+	var helper func()
+	helper = func() {
+		if len(permutation) == length {
+			temp := make([]T, length)
+			copy(temp, permutation)
+			result = append(result, temp)
+			return
+		}
+		for _, element := range elements {
+			permutation = append(permutation, element)
+			helper()
+			permutation = permutation[:len(permutation)-1]
+		}
+	}
+
+	helper()
+	return result
+}
+
 func d07_part_1(data string) (int, error) {
 	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
-	sort.Slice(lines, func(i, j int) bool {
-		return true
-	})
-	return 0, nil
+	ans := 0
+
+	for _, line := range lines {
+		ok := false
+		test_value, values := parse_result_values(line)
+		operator_permutations := generate_permutations(operators_p1, len(values)-1)
+		for _, op_perm := range operator_permutations {
+			result, err := eval_expression(values, op_perm)
+			if err != nil {
+				return 0, err
+			}
+			ok = (result == test_value)
+			if ok {
+				break
+			}
+		}
+		if ok {
+			ans += test_value
+		}
+	}
+
+	return ans, nil
 }
 
 func d07_part_2(data string) (int, error) {
 	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
-	sort.Slice(lines, func(i, j int) bool {
-		return true
-	})
-	return 0, nil
+	ans := 0
+
+	for _, line := range lines {
+		ok := false
+		test_value, values := parse_result_values(line)
+		operator_permutations := generate_permutations(operators_p2, len(values)-1)
+		for _, op_perm := range operator_permutations {
+			result, err := eval_expression(values, op_perm)
+			if err != nil {
+				return 0, err
+			}
+			ok = (result == test_value)
+			if ok {
+				break
+			}
+		}
+		if ok {
+			ans += test_value
+		}
+	}
+
+	return ans, nil
 }
